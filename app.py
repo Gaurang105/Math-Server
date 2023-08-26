4002903f95065bc91b8c400f0b0222ed21d4eb69
Gaurang105 <gujratigaurang@gmail.com>
2023-08-26 13:50:57 +0530

    math operation route added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+import pickle
 
 app = Flask(__name__)
+
+try:
+    with open('history.pkl', 'rb') as f:
+        HISTORY = pickle.load(f)
+except:
+    HISTORY = []
 
 
 def process_math(operations):
@@ -26,6 +33,28 @@ def process_math(operations):
 @app.route('/')
 def index():
     return 'Welcome to the Math Server!'
+
+
+@app.route('/<path:operation>')
+def math_operation(operation):
+    operations = operation.split('/')
+    expression, result = process_math(operations)
+    
+    if result is not None:
+        # Store in history
+        if len(HISTORY) >= 20:
+            HISTORY.pop(0)  # we'll remove the oldest operation if have 20 operations already
+        HISTORY.append({"question": expression, "answer": result})
+        
+        # saving file
+        with open('history.pkl', 'wb') as f:
+            pickle.dump(HISTORY, f)
+        
+        return jsonify(question=expression, answer=result)
+    else:
+        return jsonify(error=expression), 400
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
