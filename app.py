1d9015912820056ac1a69909e6468609281d555c
Gaurang105 <gujratigaurang@gmail.com>
2023-08-26 13:09:01 +0530

    math operations added

--- app.py
+++ app.py
@@ -2,6 +2,27 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+def process_math(operations):
+    expression = ""
+    for segment in operations:
+        if segment == 'plus':
+            expression += '+'
+        elif segment == 'minus':
+            expression += '-'
+        elif segment == 'into':
+            expression += '*'
+        elif segment == 'divided':
+            expression += '/'
+        else:
+            expression += segment
+
+    try:
+        result = eval(expression)
+        return expression, result
+    except Exception as e:
+        return str(e), None
+
 @app.route('/')
 def index():
     return 'Welcome to the Math Server!'
